93233a7b70ae1f66f581eb9f71ce3408b76904e2
octopus-knight <deepocean.2900@gmail.com>
2024-07-23 14:19:57 -0400

    delete unmerged junks

--- app/[lang]/works/[...filename]/page.tsx
+++ app/[lang]/works/[...filename]/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:app/[lang]/[...filename]/page.tsx
-import Artwork from "@/app/[lang]/[...filename]/component"
-========
 import Artwork from "@/app/[lang]/works/[...filename]/component"
->>>>>>>> develop:app/[lang]/works/[...filename]/page.tsx
 import client from "@/tina/__generated__/client"
 import { notFound } from "next/navigation"
 
